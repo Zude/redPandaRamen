7f52fc2e79b73c5d97ad39385c35cd9af1ceba2b
Alexander Löffler <alex.93.loeffler@me.com>
2019-07-03 16:22:05 +0200

    3.1

--- ueb04/red-panda-ramen/src/app/app.module.ts
+++ ueb04/red-panda-ramen/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipesComponent} from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations : [ AppComponent, RecipesComponent, RecipeDetailComponent ],
