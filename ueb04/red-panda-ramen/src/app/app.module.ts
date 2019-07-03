eb905f0c0f400279f21dd470f624266dd9fdb231
Alexander Löffler <alex.93.loeffler@me.com>
2019-07-03 15:15:11 +0200

    Step 1: The Hero Editor

--- ueb04/red-panda-ramen/src/app/app.module.ts
+++ ueb04/red-panda-ramen/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RecipesComponent} from './recipes/recipes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations : [ AppComponent, RecipesComponent ],
+  imports : [ BrowserModule, FormsModule, AppRoutingModule ],
+  providers : [],
+  bootstrap : [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule
+{}
